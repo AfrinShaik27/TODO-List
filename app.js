55148544ca3825ef9049ba03a64bfd46e881cb48
afrin shaik <skafrin7013@gmail.com>
2024-06-03 16:33:56 +0530

    todolist app

---
+++ app.js
@@ -0,0 +1,27 @@
+let array=[]
+function executetask()
+{
+let input=document.getElementById("input").value
+array.push(input)
+console.log(array)
+let containeritems= document.getElementById("containeritems")
+
+let todolistitems=document.createElement('div')
+let checkbox=document.createElement('input')
+checkbox.type='checkbox'
+let para=document.createElement('p')
+let button=document.createElement('button')
+button.innerText='delete'
+para.innerHTML=input
+todolistitems.classList.add('todo-flex')
+checkbox.classList.add('box')
+button.classList.add('button')
+
+todolistitems.appendChild(checkbox)
+todolistitems.appendChild(para)
+todolistitems.appendChild(button)
+containeritems.appendChild(todolistitems)
+
+//making input filed to be after adding the task
+document.getElementById("input").value=' '
+}
